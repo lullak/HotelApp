cd22f47902bf47e2fdd9d6c4443e3259bee03c81
lullak <dennisbircan@gmail.com>
2024-11-10 18:10:37 +0100

    migrations laptop

--- HotelApp/Repository/Migrations/20241110165932_Laptop.Designer.cs
+++ HotelApp/Repository/Migrations/20241110165932_Laptop.Designer.cs
@@ -4,6 +4,7 @@ using HotelApp.Repository;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,11 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace HotelApp.Repository.Migrations
 {
     [DbContext(typeof(HotelAppDbContext))]
-    partial class HotelAppDbContextModelSnapshot : ModelSnapshot
+    [Migration("20241110165932_Laptop")]
+    partial class Laptop
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        /// <inheritdoc />
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ HotelApp/Repository/Migrations/20241110165932_Laptop.cs
@@ -0,0 +1,80 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace HotelApp.Repository.Migrations
+{
+    /// <inheritdoc />
+    public partial class Laptop : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Invoices_InvoiceId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_InvoiceId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InvoiceId",
+                table: "Bookings",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_InvoiceId",
+                table: "Bookings",
+                column: "InvoiceId",
+                unique: true,
+                filter: "[InvoiceId] IS NOT NULL");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Invoices_InvoiceId",
+                table: "Bookings",
+                column: "InvoiceId",
+                principalTable: "Invoices",
+                principalColumn: "InvoiceId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Bookings_Invoices_InvoiceId",
+                table: "Bookings");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Bookings_InvoiceId",
+                table: "Bookings");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "InvoiceId",
+                table: "Bookings",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Bookings_InvoiceId",
+                table: "Bookings",
+                column: "InvoiceId",
+                unique: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Bookings_Invoices_InvoiceId",
+                table: "Bookings",
+                column: "InvoiceId",
+                principalTable: "Invoices",
+                principalColumn: "InvoiceId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
